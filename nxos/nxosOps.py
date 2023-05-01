7a5e3a222a3dd0ba91b78e77b3740909bb7f8869
jc3943 <jcomer@cisco.com>
2023-05-01 17:31:24 -0400

    re-sync from internal github

--- nxos/nxosOps.py
+++ nxos/nxosOps.py
@@ -2,6 +2,23 @@ import requests
 import json
 import sys, getopt, csv
 
+def nxosGetToken(specDict2, hostAddress):
+  baseURL = "http://" + hostAddress
+  tokenURL = baseURL + "/api/aaaLogin.json"
+  print(tokenURL)
+
+  #urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    
+  tokenHeader = {"content-type": "application/json","response-type": "application/json"}
+  jsonPayload = {'aaaUser':{'attributes':{'name':specDict2['username'],'pwd':specDict2['password']}}}
+  tokenResponse = requests.post(tokenURL, headers=tokenHeader, json=jsonPayload, verify=False)
+  print(tokenResponse)
+  jsonData = tokenResponse.json()
+  token = jsonData["imdata"][0]["aaaLogin"]["attributes"]["token"]
+  newCookie = {'APIC-cookie':token}
+  return newCookie
+
+
 def nxosAddFeature(specDict):
 
   with open(specDict['infile'], 'r') as csv_file:
@@ -31,5 +48,43 @@ def nxosAddFeature(specDict):
             }
           }
           response = requests.post(baseUrl,data=json.dumps(payload), headers=headers,auth=(specDict['username'],specDict['password'])).json()
+
+def nxosGetVlans(specDict):
+
+  with open(specDict['infile'], 'r') as csv_file:
+    csvread = csv.DictReader(csv_file)
+    csvDict = list(csvread)
+  keys = csvDict[0].keys()
+
+  for i in range(len(csvDict)):
+    if (csvDict[i]['mgmtIP'] != ""):
+      nxosHostToken = nxosGetToken(specDict, csvDict[i]['mgmtIP'])
+      baseUrl = "http://" + csvDict[i]['mgmtIP'] + "/api/mo/sys/bd.json?query-target=children&target-subtree-class=l2BD"
+      headers={'content-type':'application/json'}
+
+      try:
+        response = requests.get(baseUrl, headers=headers, cookies=nxosHostToken, auth=(specDict['username'],specDict['password']))
+        responseJson = response.json()
+        #print(responseJson)
+        for i in range(len(responseJson['imdata'])):
+          vlanEncap = responseJson['imdata'][i]['l2BD']['attributes']['fabEncap']
+          hostKeyList = [csvDict[i]['hostname']]
+          hostKey = hostKeyList[0]
+          print(hostKey)
+          print(vlanEncap)
+          csvDict[i][hostKey] = vlanEncap
+    
+      except requests.exceptions.Timeout:
+        print("Host " + hostIp + " Unavailable")
+
+      with open(specDict['infile'], "w", newline='') as out_file:
+        dictWrite = csv.DictWriter(out_file, keys)
+        dictWrite.writeheader()
+        dictWrite.writerows(csvDict)
+
+  return 0
+
+     
+     
 
  
